7e3c25a768d201ecd5b535722fb2fd2d501f0d7b
leonelSantiago22 <lsantiago@ittiva.com>
2024-04-07 19:36:45 -0600

    protected routes

---
+++ src/utilities/protected_routes.jsx
@@ -0,0 +1,15 @@
+import { Navigate } from "react-router-dom";
+
+export const ProtectedRoutes = ({children}) =>
+{
+    const check = window.localStorage.getItem('check');
+
+    /*if(!check)
+    {
+        return <Navigate to="/"/>
+    }*/
+    
+    return children
+}
+
+export default ProtectedRoutes;
